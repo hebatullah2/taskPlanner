5bdeb5f8d7bbb78f66f23f28df7ed5ac4bcb4f44
AGROYGBV <amandagracerenfro@gmail.com>
2021-07-13 14:58:32 -0500

    up to task 8 completed

--- js/index.js
+++ js/index.js
@@ -9,59 +9,67 @@ const statusValue = status.value;
 const email = document.getElementById("email");
 const closeDescriptionAlert = document.getElementById("closeDescriptionAlert");
 const descriptionAlert = document.getElementById("descriptionAlert");
+const taskList = document.getElementById("taskList");
+
+taskManager.load();
+taskManager.render();
+
 
 // Create a function to return the HTML for each individual task
-const createTaskHtml = (name, description, assignedTo, dueDate, statusValue, email) => {
-    const html = 
-               `<li>
-                    <section class="card text-center">
-                        <div class="card-header">
-                            <div class="float-left">
-                                Name: ${name}
-                            </div>
-                            <br>
-                            <div class="float-left">
-                                Assigned to: ${assignedTo}
-                            </div>
-                            <br>
-                            <div class="float-left">
-                                <label for="list">Status:</label>
-                                <select name="status" id ="list">
-                                    <option value="todo" placeholder="To Do" ${statusValue === 'todo'?"selected":""}>To Do</option>
-                                    <option value="in-progress" ${statusValue === 'in-progress'?"selected":""}>In-Progress</option>
-                                    <option value="done">Done</option>
-                                </select>
-                            </div>
-                        </div>
-
-                        <div class="card-body">
-                            <h5 class="card-title">Description</h5>
-                            <textarea id ="descriptionCard" class="card-text">${description}</textarea>
-                            <br>
-                            <!-- <button type="button" class="btn btn-danger">Delete</button> -->
-                            <a href="#" class="btn btn-danger">Delete</a>
-                        </div>
-
-                        <div class="card-footer">
-                            <div class="float-left">
-                                Due Date: ${dueDate}
-                            </div>
-                            <br>
-                            <div class="float-left">
-                                Email: ${email}
-                            </div>
-                         </div>
-                    </section>
-                </li>`
-    return html;
-};
+// const createTaskHtml = (name, description, assignedTo, dueDate, statusValue, email) => {
+//     const html = 
+//                `<li>
+//                     <section class="card text-center">
+//                         <div class="card-header">
+//                             <div class="float-left">
+//                                 Name: ${name}
+//                             </div>
+//                             <br>
+//                             <div class="float-left">
+//                                 Assigned to: ${assignedTo}
+//                             </div>
+//                             <br>
+//                             <div class="float-left">
+//                                 <label for="list">Status:</label>
+//                                 <select name="status" id ="list">
+//                                     <option value="todo" placeholder="To Do" ${statusValue === 'todo'?"selected":""}>To Do</option>
+//                                     <option value="in-progress" ${statusValue === 'in-progress'?"selected":""}>In-Progress</option>
+//                                     <option value="done" id="done">Done</option>
+//                                 </select>
+//                             </div>
+//                         </div>
+//                         <div class="card-body">
+//                             <h5 class="card-title">Description</h5>
+//                             <p id ="descriptionCard" class="card-text">${description}</p>
+//                            <!-- <textarea id ="descriptionCard" class="card-text">${description}</textarea>-->
+//                             <br>
+//                             <!-- <button type="button" class="btn btn-danger">Delete</button> -->
+//                             <a href="#" class="btn btn-danger">Delete</a>
+//                         </div>
+//                         <div class="card-footer">
+//                             <div class="float-left">
+//                                 Due Date: ${dueDate}
+//                             </div>
+//                             <br>
+//                             <div class="float-left">
+//                                 Email: ${email}
+//                             </div>
+//                          </div>
+//                     </section>
+//                 </li>`
+//     return html;
+// };
 // to test createTaskHtml function
 // const taskHtml = createTaskHtml('Person1','clean','Person2','07/08/2021','Todo', 'person@gmail.com');
 // console.log(taskHtml);
-
-const toggleShowAlert = () => {
-    
-}
+const resetFunction = () => {
+    taskName.value = '';
+    description.value = ''; 
+    assignedTo.value = '';
+    dueDate.value = '';
+    status.value = 'todo';
+    email.value = '';
+};
 
 const validFormFieldInput = (data) => {
    if (data.length < 1) {
@@ -71,23 +79,45 @@ const validFormFieldInput = (data) => {
         descriptionAlert.classList.remove("show");
         descriptionAlert.classList.add("hide");
     }, 5000)
-    
     return false;
     }else {
         return true;
     }
-}
-
+};
 // You can comment this (lines 82-85 below) back in and then when you click the "X" on the alert it will go away, make sure to comment out the "setTimeout" function (lines 70 - 73 above) and uncomment the "button" associated with it as well (lines 31-33 in index.html) if you want to change the way the message is closed out. Currently it will show for 3 seconds. But you can make those changes so it stays until the user closes them if you want.
 // closeDescriptionAlert.addEventListener("click", function(){
 //     descriptionAlert.classList.remove("show");
 //     descriptionAlert.classList.add("hide");
 // })
-
 save.addEventListener('click', function(event) {
     event.preventDefault();
     if (validFormFieldInput(description.value)) {
     taskManager.addTask(taskName.value, description.value, assignedTo.value, dueDate.value, status.value, email.value);
     taskManager.render();
+    resetFunction();
+    taskManager.save();
+    taskManager.load();
+    taskManager.render();
     }
 });
+
+taskList.addEventListener('click', function(event) {
+    // for (let i=0; i < taskManager.tasks.length; i++) {
+    // console.log(taskManager.tasks[i].id);
+    // }
+    const statusList = document.getElementById("list");
+    let element = event.target;
+    let currentStatus = event.target.value;
+    const done = document.getElementById("done");
+    if(element.id === 'list') {
+        if (currentStatus === 'done') {
+            list.style.backgroundColor =  '#00e600';
+        } else if (currentStatus === 'todo') {
+            list.style.backgroundColor =  '#ffcce0';
+        } else if (currentStatus === 'in-progress') {
+            list.style.backgroundColor =  '#ffff80';
+        }
+    };
+});
+
+
--- js/taskManager.js
+++ js/taskManager.js
@@ -1,21 +1,71 @@
+// Create a function to return the HTML for each individual task
+const createTaskHtml = (name, description, assignedTo, dueDate, statusValue, email) => {
+    const html = 
+               `<li>
+                    <section class="card text-center">
+                        <div class="card-header">
+                            <div class="float-left">
+                                Name: ${name}
+                            </div>
+                            <br>
+                            <div class="float-left">
+                                Assigned to: ${assignedTo}
+                            </div>
+                            <br>
+                            <div class="float-left">
+                                <label for="list">Status:</label>
+                                <select name="status" id ="list">
+                                    <option value="todo" placeholder="To Do" ${statusValue === 'todo'?"selected":""}>To Do</option>
+                                    <option value="in-progress" ${statusValue === 'in-progress'?"selected":""}>In-Progress</option>
+                                    <option value="done" id="done">Done</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">Description</h5>
+                            <p id ="descriptionCard" class="card-text">${description}</p>
+                           <!-- <textarea id ="descriptionCard" class="card-text">${description}</textarea>-->
+                            <br>
+                            <!-- <button type="button" class="btn btn-danger">Delete</button> -->
+                            <a href="#" class="btn btn-danger">Delete</a>
+                        </div>
+                        <div class="card-footer">
+                            <div class="float-left">
+                                Due Date: ${dueDate}
+                            </div>
+                            <br>
+                            <div class="float-left">
+                                Email: ${email}
+                            </div>
+                         </div>
+                    </section>
+                </li>`
+    return html;
+};
+
 //create a class that will be responsible for managing the tasks in the application 
 class TaskManager {
     constructor(currentId = '0'){
         this.tasks = [];
         this.currentId = currentId;
     }
-
     // This function creates a task
     addTask(name, description, assignedTo, dueDate, status, email = ' ') {
         this.currentId++;
         this.tasks.push({id: this.currentId , name, description, assignedTo, dueDate, status, email});
     }
-
     //Create a function to render our tasks, so that they are visible on the page
     render() {
         let tasksHtmlList = [];
         for(let i = 0; i < this.tasks.length; i++) {
             const currentTask = this.tasks[i];
+            // let formattedDate;
+            // if (dueDate.value != '') {
+            //     let date = new Date(currentTask.dueDate);
+            //     formattedDate = (date.getMonth() + 1) + '/' + (date.getDate() + 1) + '/' + date.getFullYear(); 
+            //  } else {
+            //      formattedDate = 'No due date';
+            //  };
             const date = new Date(currentTask.dueDate);
             const formattedDate = (date.getMonth() + 1) + '/' + (date.getDate() + 1) + '/' + date.getFullYear();
             const taskHtml = createTaskHtml(currentTask.name, currentTask.description, currentTask.assignedTo, formattedDate, currentTask.status, currentTask.email);
@@ -25,8 +75,26 @@ class TaskManager {
         const taskList = document.querySelector("#taskList");
         taskList.innerHTML = tasksHtml;
     }
-};
 
+    save() {
+        let tasksJson = JSON.stringify(this.tasks);
+        localStorage.setItem("tasks", tasksJson);
+        let currentId = String(this.currentId);
+        localStorage.setItem("currentId", currentId);
+    }
+
+    load() {
+        if (localStorage.getItem("tasks")) {
+            let tasksJson = localStorage.getItem("tasks");
+            this.tasks = JSON.parse(tasksJson);
+        };
+
+        if (localStorage.getItem("currentId")) {
+            let currentIdString = localStorage.getItem("currentId");
+            this.currentId = Number(currentIdString);
+        }           
+    }
+};
 // to test the code of the class
 // const task1 = new TaskManager();
 // console.log(task1.tasks);
